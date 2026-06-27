622ee1317b7db5030ce3920c4bad913d2d4bfb2a
Marcos147918 <marcosssbarrros2025@gmail.com>
2026-06-27 16:46:59 -0300

    primeiro commit

--- app.py
+++ app.py
@@ -2053,4 +2053,5 @@ if __name__ == '__main__':
     print(f"✅ {total_games} jogos cadastrados")
     db.close()
 
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
